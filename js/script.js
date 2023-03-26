e2a6e995a8b3e20539212f05ee88f5ba6d1573cc
Alessio Libbi <alex.libbi@gmail.com>
2023-03-26 16:42:47 +0200

    Add: exercise 2

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ const bikes = [
   { nome: 'Cannondale', peso: 9 },
 ];
 
-// le scorro per trovare l a meno pesante
+// le scorro per trovare la meno pesante
 let lightestBike = bikes[0];
 for (let i = 1; i < bikes.length; i++) {
   const { peso } = bikes[i];
@@ -17,3 +17,34 @@ for (let i = 1; i < bikes.length; i++) {
 
 // stampo la bici con template
 console.log(`La bici più leggera è ${lightestBike.nome} con un peso di ${lightestBike.peso} kg`);
+
+
+
+
+// SNACK DUE ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
+
+// Creo 1 array di squadre sotto forma di oggetti
+const squadre = [
+    { nome: 'Juventus', puntiFatti: 0, falliSubiti: 0 },
+    { nome: 'Milan', puntiFatti: 0, falliSubiti: 0 },
+    { nome: 'Inter', puntiFatti: 0, falliSubiti: 0 },
+    { nome: 'Roma', puntiFatti: 0, falliSubiti: 0 },
+  ];
+  
+  // Genero i numeri
+  for (let i = 0; i < squadre.length; i++) {
+    squadre[i].puntiFatti = Math.floor(Math.random() * 100) + 1;
+    squadre[i].falliSubiti = Math.floor(Math.random() * 50) + 1;
+  }
+  
+  // Creo un nuovo array 
+  const squadreFalliSubiti = [];
+  for (let i = 0; i < squadre.length; i++) {
+    const { nome, falliSubiti } = squadre[i];
+    squadreFalliSubiti.push({ nome, falliSubiti });
+  }
+  
+  // Stampo tutto in console
+  console.log('Squadre:', squadre);
+  console.log('Squadre con falli subiti:', squadreFalliSubiti);
